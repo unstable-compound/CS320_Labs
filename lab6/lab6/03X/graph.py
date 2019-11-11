b7a531fc74b2f707eb747acedce1a9c79c317b6d
Curtis Lewis <culewis@pdx.edu>
2019-11-11 12:11:36 -0800

    working on graph

--- lab6/lab6/03X/graph.py
+++ lab6/lab6/03X/graph.py
@@ -3,33 +3,72 @@
 import sys
 import os.path
 
+
 # Fill in the missing routines, using the Java versions
 # from lab4/08X and 10X as prototypes.
 #
-# read_integer_graph()
+def read_integer_graph(gname):
+    myDictGraph = dict()
+
+    filename = gname + "ig"
+    file = open(filename, "r")
+    file.readline()
+    file.readline()
+
+    line = file.readline()
+    while line != "":
+        line_list = line.split()
+        v1 = int(line_list[0])
+        v2 = int(line_list[1])
+
+        # add to v1 edge list
+        if v1 in myDictGraph:
+            e1 = myDictGraph[v1]
+        else:
+            e1 = set()
+        e1.add(v2)
+        myDictGraph[v1] = e1
+
+        # add to v2 edge list
+        if v2 in myDictGraph:
+            e2 = myDictGraph[v2]
+        else:
+            e2 = set()
+        e2.add(v1)
+        myDictGraph[v2] = e2
+
+        line = file.readline()
+
+    return myDictGraph
+
+
 #
 # read_string_graph()
 #
-# write_dot_graph()
+# def write_dot_graph(gname, graph):
+
 #
 
 
 def usage():
-  print('usage: ./graph.py [ file.ig | file.sg sep ]')    
-  exit(0)
+    print('usage: ./graph.py [ file.ig | file.sg sep ]')
+    exit(0)
+
 
 def main():
-  if len(sys.argv) < 2: usage()    
-  f = sys.argv[1]
-  gname,kind = os.path.splitext(f)
-  if kind == '.ig':
-    if len(sys.argv) != 2: usage()
-    g = read_integer_graph(gname)
-  elif kind == '.sg':
-    if len(sys.argv) != 3: usage()
-    g = read_string_graph(gname,sys.argv[2])
-  else: usage()
-  write_dot_graph(gname,g)
-  
+    if len(sys.argv) < 2: usage()
+    f = sys.argv[1]
+    gname, kind = os.path.splitext(f)
+    if kind == '.ig':
+        if len(sys.argv) != 2: usage()
+        g = read_integer_graph(gname)
+    elif kind == '.sg':
+        if len(sys.argv) != 3: usage()
+        g = read_string_graph(gname, sys.argv[2])
+    else:
+        usage()
+    write_dot_graph(gname, g)
+
+
 if __name__ == '__main__':
-  main()
+    main()
