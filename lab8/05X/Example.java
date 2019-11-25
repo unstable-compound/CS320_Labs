2f582cf05060b710858ee13b15fd8e7f54650fbf
Curtis <culewis@pdx.edu>
2019-11-25 14:04:25 -0800

    Done with lab8/05X

--- lab8/05X/Example.java
+++ lab8/05X/Example.java
@@ -11,20 +11,38 @@ class Example {
 
     System.out.println("part a:");
     int m = 
-      0; // replace this expression starting Arrays.stream(a)
+      Arrays.stream(a)
+      .max().getAsInt();
+      // replace this expression starting Arrays.stream(a)
     System.out.println(m);
 
     System.out.println("part b:");
-    // fill in here with expresssion starting Arrays.stream(a)
+    // fill in here with expression starting Arrays.stream(a)
+    int b[] = 
+      Arrays.stream(a)
+      .filter(y -> ((y-1) % 3 == 0))
+      .distinct()
+      .toArray();
+    Arrays.stream(b).forEachOrdered(x -> System.out.println(x));
+
 
     System.out.println("part c:");
     // fill in here with expresssion starting Arrays.stream(a)
+    Arrays.stream(a)
+      .dropWhile(y -> y != 0)
+      .skip(1)// skip first 0
+      .takeWhile(y -> y != 0)
+      .forEachOrdered(x -> System.out.println(x));
 
     System.out.println("part d:");
     // fill in here with expresssion starting Arrays.stream(a)
+    Arrays.stream(a)
+      .forEachOrdered(y -> IntStream.
+          range(0, y).forEach(x -> System.out.println(y)));
 
     System.out.println("part e:");
-    int last = 0; // replace this expression starting Arrays.stream(a)
+    int last = Arrays.stream(a)
+      .reduce(0, (x,y) -> y); // replace this expression starting Arrays.stream(a)
     System.out.println(last);
   }
 }
